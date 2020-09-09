d004783b73380d2b3dd75dc178a138e4cf854638
Bruno Stuyts <bruno.stuyts@cathie-associates.com>
2020-09-09 13:02:49 +0200

    Update for readthedocs online documentation

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('..'))
-from geotechnics.__version__ import __version__
+from groundhog.__version__ import __version__
 
 
 # -- Project information -----------------------------------------------------
